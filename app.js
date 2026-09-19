53b3bb7993c34efbddd93a126dbe05d827d77e08
Sajani <22cis0309@ms.sab.ac.lk>
2026-09-19 15:11:01 +0530

    Refactor fetch to retrieve data from JSONPlaceholder and update customer table rendering

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-console.log("hii");
+//console.log("hii");
 
-fetch("/Customers.json")
+fetch("https://jsonplaceholder.typicode.com/todos")
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
+
+    let customer = document.getElementById("customer-table");
+
+    let body = "";
+
+    data.forEach(element => {
+        body += `
+        <tr>
+            <td>${element.id}</td>
+            <td>${element.title}</td>
+            <td>${element.userId}</td>
+            <td>${element.completed }</td>
+        </tr>
+        `;
+        
+    });
+    customer.innerHTML = body;
   });
